7361bfc51b63c755763ea8f5126fd9f58d72c09c
Eugene The Red <me@eugenethered.com>
2022-03-29 20:02:56 +0100

    added storing + fetching of key values

--- provider/RedisCacheProvider.py
+++ provider/RedisCacheProvider.py
@@ -1,4 +1,9 @@
+from decimal import Decimal
+from typing import TypeVar
+
 import redis
+
+T = TypeVar("T")
 
 
 class RedisCacheProvider:
@@ -7,10 +12,27 @@ class RedisCacheProvider:
         self.server_address = options['REDIS_SERVER_ADDRESS']
         self.server_port = options['REDIS_SERVER_PORT']
         if auto_connect:
-            self.redis_client = redis.Redis(host=self.server_address, port=self.server_port)
+            self.redis_client = redis.Redis(host=self.server_address, port=self.server_port, decode_responses=True)
 
     def can_connect(self):
         try:
             return self.redis_client.ping()
         except redis.exceptions.ConnectionError:
             return False
+
+    def store(self, key, value):
+        if type(value) is Decimal:
+            self.redis_client.set(key, str(value))
+        else:
+            self.redis_client.set(key, value)
+
+    def fetch(self, key, as_type: T = str):
+        value = self.redis_client.get(key)
+        if as_type is int:
+            return int(value)
+        if as_type is float:
+            return float(value)
+        if as_type is Decimal:
+            return Decimal(value)
+        else:
+            return value
--- tests/provider/RedisCacheProvider_test.py
+++ tests/provider/RedisCacheProvider_test.py
@@ -1,25 +1,24 @@
 import unittest
+from decimal import Decimal
 
 from provider.RedisCacheProvider import RedisCacheProvider
 
 
 class RedisCacheProviderTestCase(unittest.TestCase):
 
-    def test_should_set_server_options(self):
-        options = {
+    def setUp(self):
+        self.options = {
             'REDIS_SERVER_ADDRESS': '192.168.1.90',
             'REDIS_SERVER_PORT': 6379
         }
-        cache_provider = RedisCacheProvider(options, auto_connect=False)
+
+    def test_should_set_server_options(self):
+        cache_provider = RedisCacheProvider(self.options, auto_connect=False)
         self.assertEqual(cache_provider.server_address, '192.168.1.90')
         self.assertEqual(cache_provider.server_port, 6379)
 
     def test_should_connect_to_redis_server(self):
-        options = {
-            'REDIS_SERVER_ADDRESS': '192.168.1.90',
-            'REDIS_SERVER_PORT': 6379
-        }
-        cache_provider = RedisCacheProvider(options)
+        cache_provider = RedisCacheProvider(self.options)
         connected = cache_provider.can_connect()
         self.assertEqual(connected, True)
 
@@ -31,6 +30,31 @@ class RedisCacheProviderTestCase(unittest.TestCase):
         cache_provider = RedisCacheProvider(options)
         connected = cache_provider.can_connect()
         self.assertEqual(connected, False)
+
+    def test_should_store_key_string_value(self):
+        cache_provider = RedisCacheProvider(self.options)
+        cache_provider.store('foo', 'bar')
+        value = cache_provider.fetch('foo')
+        self.assertEqual(value, 'bar')
+
+    def test_should_store_key_integer_value(self):
+        cache_provider = RedisCacheProvider(self.options)
+        cache_provider.store('test-number', 10)
+        value = cache_provider.fetch('test-number', as_type=int)
+        self.assertEqual(value, 10)
+
+    def test_should_store_key_simple_float_value(self):
+        cache_provider = RedisCacheProvider(self.options)
+        cache_provider.store('test-float', 100.12)
+        value = cache_provider.fetch('test-float', as_type=float)
+        self.assertEqual(value, 100.12)
+
+    def test_should_store_key_large_precision_float_value(self):
+        cache_provider = RedisCacheProvider(self.options)
+        cache_provider.store('test-large-float', Decimal(1000000000.123456789012))
+        value = cache_provider.fetch('test-large-float', as_type=Decimal)
+        self.assertAlmostEqual(value, 1000000000.123456789012, places=12)
+        self.fail('Large precision fails (rounding + significant places)!')
 
 
 if __name__ == '__main__':
