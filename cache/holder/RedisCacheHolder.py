f21c213fb0e63eea15a0040e19d15fe70f946f85
Eugene The Red <me@eugenethered.com>
2022-04-05 17:01:28 +0100

    ensuring subsequent calls needn't options

--- cache/holder/RedisCacheHolder.py
+++ cache/holder/RedisCacheHolder.py
@@ -4,7 +4,7 @@ from cache.provider.RedisCacheProvider import RedisCacheProvider
 class RedisCacheHolder:
     __instance = None
 
-    def __new__(cls, options):
+    def __new__(cls, options=None):
         if cls.__instance is None:
             auto_connect = True if 'AUTO_CONNECT' not in options else options['AUTO_CONNECT']
             cls.__instance = RedisCacheProvider(options, auto_connect)
--- tests/cache/holder/RedisCacheHolder_test.py
+++ tests/cache/holder/RedisCacheHolder_test.py
@@ -5,7 +5,7 @@ from cache.holder.RedisCacheHolder import RedisCacheHolder
 
 class RedisCacheHolderTestCase(unittest.TestCase):
 
-    def test_should_initialize_redis_cache_provider(self):
+    def test_should_initialize_only_one_redis_cache_provider_instance(self):
         options = {
             'REDIS_SERVER_ADDRESS': '192.168.1.90',
             'REDIS_SERVER_PORT': 6379,
@@ -15,6 +15,18 @@ class RedisCacheHolderTestCase(unittest.TestCase):
         self.assertIsNotNone(cache_holder)
         instance_1 = id(RedisCacheHolder(options))
         instance_2 = id(RedisCacheHolder(options))
+        self.assertEqual(instance_1, instance_2, 'every instance after should be the same')
+
+    def test_should_not_require_redis_cache_provider_options_on_subsequent_calls(self):
+        options = {
+            'REDIS_SERVER_ADDRESS': '192.168.1.90',
+            'REDIS_SERVER_PORT': 6379,
+            'AUTO_CONNECT': False
+        }
+        cache_holder = RedisCacheHolder(options)
+        self.assertIsNotNone(cache_holder)
+        instance_1 = id(RedisCacheHolder())
+        instance_2 = id(RedisCacheHolder())
         self.assertEqual(instance_1, instance_2, 'every instance after should be the same')
 
 
