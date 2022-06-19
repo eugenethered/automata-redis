c2e3780604f6035a04e2868592f6a903a2e2aa71
Eugene the Red <me@eugenethered.com>
2022-06-19 22:15:10 +0100

    store + fetch list

--- cache/provider/RedisCacheProviderWithHash.py
+++ cache/provider/RedisCacheProviderWithHash.py
@@ -1,6 +1,11 @@
 import logging
+from typing import TypeVar
+
+from coreutility.json.json_utility import as_pretty_json, as_json
 
 from cache.provider.RedisCacheProvider import RedisCacheProvider
+
+T = TypeVar("T")
 
 
 class RedisCacheProviderWithHash(RedisCacheProvider):
@@ -9,11 +14,21 @@ class RedisCacheProviderWithHash(RedisCacheProvider):
         self.log = logging.getLogger('RedisCacheProviderWithHash')
         super().__init__(options, auto_connect)
 
-    def store_values(self, key, values: dict):
+    def store_values(self, key, values):
         self.log.debug(f'storing values for key:{key}')
-        for k, v in values.items():
-            self.redis_client.hset(key, k, v)
+        if type(values) is dict:
+            for k, v in values.items():
+                self.redis_client.hset(key, k, v)
+        elif type(values) is list:
+            for v in values:
+                value_first_key = next(iter(v))
+                serialized_value = as_pretty_json(v, indent=None)
+                self.redis_client.hset(key, value_first_key, serialized_value)
 
-    def fetch_values(self, key):
+    def fetch_values(self, key, as_type: T = dict):
         self.log.debug(f'fetching values for key:{key}')
-        return self.redis_client.hgetall(key)
+        if as_type is dict:
+            return self.redis_client.hgetall(key)
+        elif as_type is list:
+            stored_values = self.redis_client.hgetall(key)
+            return list([as_json(v) for k, v in stored_values.items()])
--- tests/cache/provider/RedisCacheProviderWithHash_test.py
+++ tests/cache/provider/RedisCacheProviderWithHash_test.py
@@ -19,6 +19,7 @@ class RedisCacheProviderWithHashTestCase(unittest.TestCase):
         cache_provider = RedisCacheProviderWithHash(self.options)
         cache_provider.delete('test:values:dictionary-simple')
         cache_provider.delete('test:values:dictionary-simple-multiple')
+        cache_provider.delete('test:values:dictionary-multiple-simple-list')
 
     def test_should_store_list_of_values_by_each_key(self):
         cache_provider = RedisCacheProviderWithHash(self.options)
@@ -33,6 +34,18 @@ class RedisCacheProviderWithHashTestCase(unittest.TestCase):
         cache_provider.store_values('test:values:dictionary-simple-multiple', values_to_store)
         values = cache_provider.fetch_values('test:values:dictionary-simple-multiple')
         self.assertEqual(values, {'A': '1', 'B': '2'}, 'should convert to string values!')
+
+    def test_should_store_list_of_values_using_specified_key(self):
+        cache_provider = RedisCacheProviderWithHash(self.options)
+        values_to_store = [
+            {'A': '1'},
+            {'B': '2'},
+            {'C': '3'}
+        ]
+        cache_provider.store_values('test:values:dictionary-multiple-simple-list', values_to_store)
+        values = cache_provider.fetch_values('test:values:dictionary-multiple-simple-list', as_type=list)
+        print(f'values returned! -> {values}')
+        self.assertEqual(values, values_to_store)
 
 
 if __name__ == '__main__':
