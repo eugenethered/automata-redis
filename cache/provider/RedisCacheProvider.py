aa269ee603a2f149659e020342d5030ce8276440
Eugene the Red <me@eugenethered.com>
2022-06-19 21:26:07 +0100

    separate converns

--- cache/provider/RedisCacheProvider.py
+++ cache/provider/RedisCacheProvider.py
@@ -25,7 +25,6 @@ class RedisCacheProvider:
             self.server_port = options[REDIS_SERVER_PORT]
             self.log.info(f'Connecting to REDIS server {self.server_address}:{self.server_port}')
             self.redis_client = redis.Redis(host=self.server_address, port=self.server_port, decode_responses=True)
-            self.redis_timeseries = self.redis_client.ts()
 
     def __check_options(self):
         if self.options is None:
@@ -61,16 +60,8 @@ class RedisCacheProvider:
             self.log.debug(f'default storing key:{key} [{value}]')
             return self.redis_client.set(key, value)
 
-    def overwrite_store(self, key, values):
-        self.log.debug(f'overwrite values:[{len(values)}] for key:[{key}]')
-        delete_result = self.delete(key)
-        self.log.debug(f'overwrite delete result:[{delete_result}] for key:[{key}]')
-        store_result = self.store(key, values)
-        self.log.debug(f'overwrite store result:[{store_result}] for key:[{key}]')
-
     def fetch(self, key, as_type: T = str):
         value = self.redis_client.get(key)
-        self.log.debug(f'fetching for key:{key} value:[{len(value)}] (parse later)')
         if value is not None and value == NOT_AVAILABLE:
             return NOT_AVAILABLE
         if as_type is int:
@@ -92,65 +83,3 @@ class RedisCacheProvider:
 
     def delete(self, key):
         return self.redis_client.delete(key)
-
-    @staticmethod
-    def fraction_key(key):
-        return f'{key}:fraction'
-
-    @staticmethod
-    def fraction_leading_zeros_key(key):
-        return f'{key}:fraction:leading-zeros'
-
-    def __create_timeseries(self, key, field_name, limit_retention):
-        if not self.does_timeseries_exist(key):
-            self.redis_timeseries.create(key, labels={'time': field_name}, retention_msecs=limit_retention)
-
-    def create_timeseries(self, key, field_name, double_precision=False, limit_retention=0):
-        self.__create_timeseries(key, field_name, limit_retention)
-        if double_precision:
-            self.__create_timeseries(self.fraction_key(key), field_name, limit_retention)
-            self.__create_timeseries(self.fraction_leading_zeros_key(key), field_name, limit_retention)
-
-    def add_to_timeseries(self, key, time, value):
-        if type(value) is BigFloat:
-            self.redis_timeseries.add(key, time, value.number)
-            self.redis_timeseries.add(self.fraction_key(key), time, value.fraction)
-            self.redis_timeseries.add(self.fraction_leading_zeros_key(key), time, value.fraction_leading_zeros)
-        else:
-            self.redis_timeseries.add(key, time, value)
-
-    def get_timeseries_data(self, key, time_from, time_to, double_precision=False, reverse_direction=False):
-        if double_precision:
-            if reverse_direction is False:
-                number_values = self.redis_timeseries.range(key, time_from, time_to)
-                fraction_values = self.redis_timeseries.range(self.fraction_key(key), time_from, time_to)
-                fraction_leading_zero_values = self.redis_timeseries.range(self.fraction_leading_zeros_key(key), time_from, time_to)
-                return [(n1, BigFloat(int(v1), int(v2), int(v3))) for (n1, v1), (f2, v2), (l3, v3) in zip(number_values, fraction_values, fraction_leading_zero_values) if n1 == f2 and n1 == l3]
-            else:
-                number_values = self.redis_timeseries.revrange(key, time_from, time_to)
-                fraction_values = self.redis_timeseries.revrange(self.fraction_key(key), time_from, time_to)
-                fraction_leading_zero_values = self.redis_timeseries.revrange(self.fraction_leading_zeros_key(key), time_from, time_to)
-                return [(n1, BigFloat(int(v1), int(v2), int(v3))) for (n1, v1), (f2, v2), (l3, v3) in zip(number_values, fraction_values, fraction_leading_zero_values) if n1 == f2 and n1 == l3]
-        else:
-            if reverse_direction is False:
-                return self.redis_timeseries.range(key, time_from, time_to)
-            else:
-                return self.redis_timeseries.revrange(key, time_from, time_to)
-
-    def does_timeseries_exist(self, timeseries_key):
-        try:
-            self.redis_timeseries.info(timeseries_key)
-            return True
-        except redis.exceptions.ResponseError:
-            return False
-
-    def delete_timeseries(self, key, double_precision=False):
-        self.delete(key)
-        if double_precision:
-            self.delete(self.fraction_key(key))
-            self.delete(self.fraction_leading_zeros_key(key))
-
-    def get_timeseries_retention_time(self, timeseries_key):
-        info = self.redis_timeseries.info(timeseries_key)
-        return info.retention_msecs
-
--- tests/cache/provider/RedisCacheProvider_test.py
+++ tests/cache/provider/RedisCacheProvider_test.py
@@ -1,9 +1,7 @@
 import logging
 import unittest
-from datetime import datetime
 
 from core.constants.not_available import NOT_AVAILABLE
-from core.instant.RunInstantHolder import RunInstantHolder
 from core.number.BigFloat import BigFloat
 from redis.exceptions import DataError
 
@@ -32,10 +30,6 @@ class RedisCacheProviderTestCase(unittest.TestCase):
         cache_provider.delete('test-float')
         cache_provider.delete('test-list')
         cache_provider.delete('test:test-list-json')
-        cache_provider.delete_timeseries('timeseries-test')
-        cache_provider.delete_timeseries('timeseries-big-float-test', double_precision=True)
-        cache_provider.delete_timeseries('test-timeseries-limited-retention')
-        cache_provider.delete_timeseries('test-timeseries-big-float-limited-retention', double_precision=True)
 
     def test_should_connect_to_redis_server(self):
         cache_provider = RedisCacheProvider(self.options)
@@ -86,99 +80,6 @@ class RedisCacheProviderTestCase(unittest.TestCase):
         value = cache_provider.fetch('test-na-float', as_type=float)
         self.assertEqual(value, NOT_AVAILABLE)
 
-    def test_should_store_key_large_precision_float_value(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.store('test-big-float', BigFloat('1000000000.123456789012'))
-        value = cache_provider.fetch('test-big-float', as_type=BigFloat)
-        self.assertEqual(str(value), '1000000000.123456789012')
-
-    def test_should_store_key_large_precision_float_with_fraction_leading_zeros_value(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.store('test-big-float', BigFloat('1000000000.000000000012'))
-        value = cache_provider.fetch('test-big-float', as_type=BigFloat)
-        self.assertEqual(str(value), '1000000000.000000000012')
-
-    def test_should_store_time_series(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-test', 'price')
-        cache_provider.add_to_timeseries('timeseries-test', 1, 10.00)
-        cache_provider.add_to_timeseries('timeseries-test', 2, 11.00)
-        cache_provider.add_to_timeseries('timeseries-test', 3, 12.00)
-        timeseries_data = cache_provider.get_timeseries_data('timeseries-test', time_from=1, time_to=3)
-        expected = [(1, 10.0), (2, 11.0), (3, 12.0)]
-        self.assertEqual(expected, timeseries_data)
-
-    def test_should_obtain_time_series_in_reverse_direction(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-test', 'price')
-        cache_provider.add_to_timeseries('timeseries-test', 1, 10.00)
-        cache_provider.add_to_timeseries('timeseries-test', 2, 11.00)
-        cache_provider.add_to_timeseries('timeseries-test', 3, 12.00)
-        timeseries_data = cache_provider.get_timeseries_data('timeseries-test', time_from=1, time_to=3, reverse_direction=True)
-        expected = [(3, 12.0), (2, 11.0), (1, 10.0)]
-        self.assertEqual(expected, timeseries_data)
-
-    def test_should_not_fail_when_attempting_to_create_timeseries_multiple_times(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-test', 'price')
-        result = cache_provider.does_timeseries_exist('timeseries-test')
-        self.assertTrue(result, 'Timeseries should be created')
-        cache_provider.create_timeseries('timeseries-test', 'price')
-        cache_provider.create_timeseries('timeseries-test', 'price')
-        self.assertTrue(result, 'Timeseries should already have been created')
-
-    def test_should_delete_timeseries(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-test', 'price')
-        result = cache_provider.does_timeseries_exist('timeseries-test')
-        self.assertTrue(result, 'Timeseries should be created')
-        cache_provider.delete_timeseries('timeseries-test')
-        result = cache_provider.does_timeseries_exist('timeseries-test')
-        self.assertFalse(result, 'Timeseries should not exist')
-
-    def test_should_store_time_series_with_big_floats(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-big-float-test', 'price', double_precision=True)
-        cache_provider.add_to_timeseries('timeseries-big-float-test', 1, BigFloat('1000000000.123456789012'))
-        cache_provider.add_to_timeseries('timeseries-big-float-test', 2, BigFloat('2000000000.210987654321'))
-        timeseries_data = cache_provider.get_timeseries_data('timeseries-big-float-test', time_from=1, time_to=2, double_precision=True)
-        expected = [(1, BigFloat('1000000000.123456789012')), (2, BigFloat('2000000000.210987654321'))]
-        self.assertEqual(expected, timeseries_data)
-
-    def test_should_store_time_series_with_big_floats_in_reverse(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-big-float-test', 'price', double_precision=True)
-        cache_provider.add_to_timeseries('timeseries-big-float-test', 1, BigFloat('1000000000.123456789012'))
-        cache_provider.add_to_timeseries('timeseries-big-float-test', 2, BigFloat('2000000000.210987654321'))
-        timeseries_data = cache_provider.get_timeseries_data('timeseries-big-float-test', time_from=1, time_to=2, double_precision=True, reverse_direction=True)
-        expected = [(2, BigFloat('2000000000.210987654321')), (1, BigFloat('1000000000.123456789012'))]
-        self.assertEqual(expected, timeseries_data)
-
-    def test_should_store_time_series_with_big_floats_having_leading_zeros(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-big-float-test', 'price', double_precision=True)
-        cache_provider.add_to_timeseries('timeseries-big-float-test', 1, BigFloat('1000000000.000000000012'))
-        cache_provider.add_to_timeseries('timeseries-big-float-test', 2, BigFloat('2000000000.010987654321'))
-        timeseries_data = cache_provider.get_timeseries_data('timeseries-big-float-test', time_from=1, time_to=2, double_precision=True)
-        expected = [(1, BigFloat('1000000000.000000000012')), (2, BigFloat('2000000000.010987654321'))]
-        self.assertEqual(expected, timeseries_data)
-
-    def test_should_store_time_series_with_mixed_big_floats_to_millisecond_times(self):
-        cache_provider = RedisCacheProvider(self.options)
-        cache_provider.create_timeseries('timeseries-big-float-test', 'price', double_precision=True)
-
-        RunInstantHolder.initialize(datetime.fromisoformat('2021-04-01T21:16:25.919601+00:00'))
-        time_interval_1 = RunInstantHolder.numeric_run_instance('')
-        cache_provider.add_to_timeseries('timeseries-big-float-test', time_interval_1, BigFloat('1000000000.123456789012'))
-
-        RunInstantHolder.initialize(datetime.fromisoformat('2021-04-01T21:16:25.919605+00:00'))
-        time_interval_2 = RunInstantHolder.numeric_run_instance('')
-        cache_provider.add_to_timeseries('timeseries-big-float-test', time_interval_2, BigFloat('1000000000.000000000012'))
-
-        timeseries_data = cache_provider.get_timeseries_data('timeseries-big-float-test', time_from=time_interval_1, time_to=time_interval_2, double_precision=True)
-        expected = [(1617311785919601, BigFloat('1000000000.123456789012')), (1617311785919605, BigFloat('1000000000.000000000012'))]
-        self.assertEqual(expected, timeseries_data)
-
     def test_should_store_json_data(self):
         config = {
             'name': 'Eugene',
@@ -226,28 +127,6 @@ class RedisCacheProviderTestCase(unittest.TestCase):
         keys = cache_provider.get_keys('test-*')
         self.assertGreater(len(keys), 0)
 
-    def test_should_store_time_series_with_limited_retention(self):
-        cache_provider = RedisCacheProvider(self.options)
-        # limit to 100 ms
-        timeseries_key = 'test-timeseries-limited-retention'
-        cache_provider.create_timeseries(timeseries_key, 'price', limit_retention=100)
-        cache_provider.add_to_timeseries(timeseries_key, '*', 10.00)
-        cache_provider.add_to_timeseries(timeseries_key, '*', 11.00)
-        cache_provider.add_to_timeseries(timeseries_key, '*', 12.00)
-        retention_time = cache_provider.get_timeseries_retention_time(timeseries_key)
-        self.assertEqual(retention_time, 100)
-
-    def test_should_create_double_precision_time_series_all_with_limited_retention(self):
-        cache_provider = RedisCacheProvider(self.options)
-        timeseries_key = 'test-timeseries-big-float-limited-retention'
-        cache_provider.create_timeseries(timeseries_key, 'price', double_precision=True, limit_retention=100)
-        retention_time = cache_provider.get_timeseries_retention_time(timeseries_key)
-        self.assertEqual(retention_time, 100)
-        fraction_retention_time = cache_provider.get_timeseries_retention_time(cache_provider.fraction_key(timeseries_key))
-        self.assertEqual(fraction_retention_time, 100)
-        leading_zero_fraction_retention_time = cache_provider.get_timeseries_retention_time(cache_provider.fraction_leading_zeros_key(timeseries_key))
-        self.assertEqual(leading_zero_fraction_retention_time, 100)
-
     def test_should_retrieve_not_found_dict_data_as_none(self):
         cache_provider = RedisCacheProvider(self.options)
         dictionary_value = cache_provider.fetch('test-empty-value', as_type=dict)
@@ -259,20 +138,18 @@ class RedisCacheProviderTestCase(unittest.TestCase):
         self.assertIsNotNone(list_value)
         self.assertTrue(len(list_value) == 0)
 
-    def test_should_overwrite_values(self):
+    def test_should_store_key_large_precision_float_value(self):
         cache_provider = RedisCacheProvider(self.options)
-        values = [
-            {"test": "A", "context": "C1", "other": "111", "description": "Storing Value 1"},
-            {"test": "B", "context": "C1", "other": "101", "description": "Storing Value 2"},
-            {"test": "C", "context": "C1", "other": "100", "description": "Storing Value 3"}
-        ]
-        cache_provider.store('test:test-list-json', values)
-        stored_values = cache_provider.fetch('test:test-list-json', as_type=list)
-        self.assertEqual(len(stored_values), 3)
-        values_without_removed = [v for v in values if v['test'] != 'B']
-        cache_provider.overwrite_store('test:test-list-json', values_without_removed)
-        stored_values_check = cache_provider.fetch('test:test-list-json', as_type=list)
-        self.assertEqual(len(stored_values_check), 2)
+        cache_provider.store('test-big-float', BigFloat('1000000000.123456789012'))
+        value = cache_provider.fetch('test-big-float', as_type=BigFloat)
+        self.assertEqual(str(value), '1000000000.123456789012')
+
+    def test_should_store_key_large_precision_float_with_fraction_leading_zeros_value(self):
+        cache_provider = RedisCacheProvider(self.options)
+        cache_provider.store('test-big-float', BigFloat('1000000000.000000000012'))
+        value = cache_provider.fetch('test-big-float', as_type=BigFloat)
+        self.assertEqual(str(value), '1000000000.000000000012')
+
 
 
 if __name__ == '__main__':
